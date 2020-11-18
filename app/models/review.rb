5dbfe253e33fc8a74f4a878add3a12975da98dc4
Ludovic Villain <chadludo@gmail.com>
2020-11-18 15:35:22 +0100

    all good

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
+  RATING = (0..5)
+
   validates :rating, presence: true
   validates :rating, :inclusion => 0..5
   validates :rating, numericality: { only_integer: true }
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "restaurants", to: "restaurants#index"
-  get "restaurants/:id", to: "restaurants#show", as: :restaurant
-  get "new", to: "restaurants#new", as: :new_restaurant
-  post "restaurants", to: "restaurants#create"
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # get "new", to: "restaurants#new", as: :new_restaurant
+  # post "restaurants", to: "restaurants#create"
+  resources :restaurants, only: [:index, :show, :create, :new] do
+    resources :reviews, only: [:new, :create]
+  end
 end
